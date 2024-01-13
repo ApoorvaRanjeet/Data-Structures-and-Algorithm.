6b59cf5555600a0aa5fe8742f29429bee573718c
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2024-01-13 07:35:47 +0530

    Add files via upload

---
+++ Array/FindfirstlastPosition.java
@@ -0,0 +1,57 @@
+public class FindfirstlastPosition {
+    public int[] position(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirstPosition(nums, target);
+        result[1] = findLastPosition(nums, target);
+        return result;
+    }
+
+    private int findFirstPosition(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    return mid;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int findLastPosition(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    return mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FindfirstlastPosition finder = new FindfirstlastPosition();
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] ans = finder.position(nums, target);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+}
+
