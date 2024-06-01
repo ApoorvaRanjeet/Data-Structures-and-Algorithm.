0cb2ed908177744fcde748d18e1489b71fb2d81d
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2024-06-01 19:01:06 +0530

    Add files via upload

---
+++ Binary Search Tree/lowestcommancestor.java
@@ -0,0 +1,38 @@
+public class lowestcommancestor
+{
+    class Node
+{
+    Node root;
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data=data;
+        left=right=null;
+    }
+    Node lca(Node root,int n1,int n2)
+    {
+        if(root==null || root.data==n1 || root.data==n2)
+        {
+            return root;
+        }
+        Node left=lca(root.left,n1,n2);
+        Node right=lca(root.right,n1,n2);
+        
+        if(left==null)
+        {
+            return right;
+        }
+        else if(right==null)
+        {
+            return left;
+        }
+        else
+        {
+            return root;
+        }
+    }
+}
+    
+}
