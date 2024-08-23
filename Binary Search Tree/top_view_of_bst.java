553b586f2aa48b5d464130bead9b4c79731410a3
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2024-08-23 19:55:23 +0530

    Add files via upload

---
+++ Binary Search Tree/top_view_of_bst.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class top_view_of_bst 
+{
+    static Node root;
+    static class Node
+    {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value)
+        {
+            this.value = value;
+        }
+    }
+    public List<Integer> top(Node root)
+    {
+        List<Integer> ans= new ArrayList<>();
+        if(root==null)
+        {
+            return ans;
+        }
+        Map<Integer,Integer> mpp= new TreeMap<>();
+        Queue<Pair<Node, Integer>> q = new LinkedList<>();
+        q.add(new Pair<>(root,0));
+        while(!q.isEmpty())
+        {
+            Pair<Node,Integer> p= q.poll();
+            Node node=p.getValue();
+            int line=p.getKey();
+            
+            if(!mpp.containsKey(line))
+            {
+                mpp.put(line,node.value);
+            }
+            if(node.left!=null)
+            {
+                q.add(new Pair<>(node.left,line-1));
+            }
+            if(node.right!=null)
+            {
+                q.add(new Pair<>(node.right,line+1));
+            }
+        }
+        for(int value:mpp.values())
+        {
+            ans.add(value);
+        }
+        return ans;
+    }
+}
