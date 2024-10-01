11177571d7023ccc0bdd083b51b77648d86dbd6d
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2024-10-01 10:09:54 +0530

    Add files via upload

---
+++ Dynamic Programming/counting_bits.java
@@ -0,0 +1,13 @@
+public class counting_bits 
+{
+    int[] counting(int n)
+    {
+        int[]ans= new int[n+1];
+        for(int i=1;i<=n;i++)
+        {
+            ans[i]=ans[i>>1]+(i&1);
+        }
+        return ans;
+    }
+    
+}
