faebdcf12e6406472b90d09152f6d3c2bba394d0
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2024-11-19 16:59:00 +0530

    Add files via upload

---
+++ geedy algo/valid_parentheses_string.java
@@ -0,0 +1,30 @@
+// import java.util.*;
+public class valid_parentheses_string 
+{
+    public boolean valid(String s)
+    {
+        int minopen=0;
+        int maxopen=0;
+        for(char c:s.toCharArray())
+        {
+            if(c=='(')
+            {
+                minopen++;
+                maxopen--;
+            }
+            else if(c==')')
+            {
+                minopen=Math.max(minopen-1,0);
+                maxopen--;
+            }
+            else if(c=='*')
+            {
+                minopen=Math.min(minopen-1,0);
+                maxopen++;
+            }
+        }
+        if(maxopen<0)return false;
+        return minopen==0;
+    }
+    
+}
