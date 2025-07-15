5f2d9975d6f2a9855e351386e8f4a3d27b6bfd0e
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2025-07-15 20:35:01 +0530

    Add files via upload

--- MyDSAApproach/two_pointers/move_zeros_to_end.java
+++ MyDSAApproach/two_pointers/move_zeros_to_end.java
@@ -1,29 +1,26 @@
-public class move_zeros_to_end 
+import java.util.*;
+
+public class move_zeros_to_end
 {
-    public static int[] zeros(int[]arr)
+    public static int[] movezeros(int[]arr)
     {
-        if(arr.length==0)
+        int n=arr.length;
+        int index=0;
+        for(int i=0;i<n;i++)
         {
-            return null;
-        }
-        int l=0;
-        for(int r=0;r<arr.length;r++)
-        {
-            if(arr[r]!=0)
+            if(arr[i]!=0)
             {
-                int temp=arr[l];
-                arr[l]=arr[r];
-                arr[r]=temp;
-                l++;
+                int temp=arr[i];
+                arr[i]=arr[index];
+                arr[index]=temp;
+                index++;
             }
         }
-return arr;
+        return arr;
     }
-    public static void main(String[] args) {
-        int[] arr = {0,1,0,3,12};
-        int[] result = zeros(arr);
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
-        }
+    public static void main(String[]args)
+    {
+        int[]arr={1,0,2,3,0};
+        System.out.print(Arrays.toString(movezeros(arr)));
     }
 }
